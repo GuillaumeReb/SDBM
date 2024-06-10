d971dd90e431856542662213c8c3b0c79eabb521
GuillaumeReb <162027291+GuillaumeReb@users.noreply.github.com>
2024-06-10 10:32:30 +0200

    Add files via upload

---
+++ SDBM_A_DEUX/SDBM/views/couleurs/ajout.php
@@ -0,0 +1,3 @@
+<h1>Bienvenue sur la page de demo AJOUT</h1>
+
+<h2><?php echo @$montitre2; ?></h2>
---
+++ SDBM_A_DEUX/SDBM/views/couleurs/supprime.php
@@ -0,0 +1,3 @@
+<h1>Bienvenue sur la page de demo SUPPRESSION</h1>
+
+<h2><?php echo @$montitre2; ?></h2>
