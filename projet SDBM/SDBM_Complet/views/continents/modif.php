8261e6f372038d3b14d39a84564927ddacfecc18
AO1289 <abdellahe.ouammou@outlook.com>
2024-06-10 23:17:07 +0200

    Modif continent fonctionne completement

--- projet SDBM/SDBM_Complet/views/continents/modif.php
+++ projet SDBM/SDBM_Complet/views/continents/modif.php
@@ -1,13 +1,16 @@
-<h1>Modification d'un continent</h1>
-<form action="<?= PATH ?>/continents/modif_sauve/<?php echo $continent["ID_CONTINENT"]; ?>" method="POST">
-<div class="mb-3">
-    <label for="Id" class="form-label">Code Continent</label>
-    <input type="number" class="form-control" id="Id" name="Id" value="<?php echo $continent['ID_CONTINENT']; ?>" disabled>
-  </div>
-<div class="mb-3">
-    <label for="Nom" class="form-label">Continent</label>
-    <input type="text" class="form-control" id="Nom" name="Nom" value="<?php echo $continent['NOM_CONTINENT']; ?>">
-  </div>
-  <input type="submit" class="btn btn-primary">
-</form>
+<h1>Modification d'un Continent</h1>
+
+<form action="<?= PATH ?>/continents/modif_sauve/<?= $continent['ID_CONTINENT'] ?>" method="POST">
+        <div class="form-group">
+          <label for="Id">Code Continent :</label>
+          <input type="text" class="form-control" placeholder="Saisir un Code" name="Id" id="Id"
+          value=<?= $continent['ID_CONTINENT'] ?> readonly>
+        </div>
+        <div class="form-group">
+          <label for="Nom">Nom Continent:</label>
+          <input type="text" class="form-control" placeholder="Saisir un Nom" name="Nom" id="Nom"
+          value=<?= $continent['NOM_CONTINENT'] ?>>
+        </div>
+        <button type="submit" class="btn btn-primary">Enregistrer</button>
+</form>  
 <a href="<?= PATH ?>/continents"><button  class="btn btn-warning">Retour à la liste</button></a>
