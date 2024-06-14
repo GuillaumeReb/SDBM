8ed99ae64d11fe41341b48af7f9face1a494e70b
AO1289 <abdellahe.ouammou@outlook.com>
2024-06-14 12:05:23 +0200

    correction type

--- views/tickets/ajout.php
+++ views/tickets/ajout.php
@@ -1,9 +1,10 @@
-<h1>Ajout d'un ticket</h1>
+<main class="container vh-100"></main>
+<h1 class="mb-5 mt-5">Ajout d'un ticket</h1>
 <form action="<?= PATH ?>/tickets/ajout_sauve" method="POST">
         <div class="form-group">
-          <label for="Nom">Année :</label>
-          <input type="text" class="form-control" placeholder="Saisir une Année" name="Annee" id="Nom"> 
+          <label for="Nom" class="form-label">Année :</label>
+          <input type="text" class="form-control mb-3" placeholder="Saisir une Année" name="Annee" id="Nom"> 
         </div>
-        <button type="submit" class="btn btn-primary">Ajouter</button>
+        <button type="submit" class="btn btn-primary mb-2 mt-3">Ajouter</button>
 </form>
 <a href="<?= PATH ?>/tickets"><button  class="btn btn-warning">Retour à la liste</button></a>
