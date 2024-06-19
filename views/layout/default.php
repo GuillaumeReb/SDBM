62df97adb1e37ffb393e76168c4ca9638b0b7cc6
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-06-19 11:05:20 +0200

    Nouveau favicon

--- views/layout/default.php
+++ views/layout/default.php
@@ -5,7 +5,7 @@
     <title><?php echo @$Titre; ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="./images/beer-shop.png" type="image/png">
+    <link rel="shortcut icon" href="./images/beer.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
     crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
